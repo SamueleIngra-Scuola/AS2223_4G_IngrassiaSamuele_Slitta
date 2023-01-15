c518aaa24565a7ffb183baa31c4ae54336d7e8da
S4pphiree <s4pph1r3sam@gmail.com>
2023-01-15 23:42:46 +0100

    feat: Add Sleigh.cs and Gift.cs Classes

---
+++ Classes/Gift.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Gift
+    {
+        public string name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string colour {
+            get
+            {
+                return colour;
+            }
+
+            set
+            {
+                colour = value;
+            }
+        }
+
+        public double weight
+        {
+            get
+            {
+                return weight;
+            }
+
+            set
+            {
+                weight = value;
+            }
+        }
+
+        public double volume
+        {
+            get
+            {
+                return volume;
+            }
+
+            set
+            {
+                volume = value;
+            }
+        }
+
+        public Gift() { }
+
+        public Gift(string color, double weight, double volume) 
+        {
+            this.colour = color;
+            this.weight = weight;
+            this.volume = volume;
+        }
+
+    }
+}
--- Classes/Sleigh.cs
+++ Classes/Sleigh.cs
@@ -2,6 +2,47 @@
 {
     public class Sleigh
     {
-        
+        public string sleighName
+        {
+            get
+            {
+                return sleighName;
+            }
+            set
+            {
+                sleighName = value;
+            }
+        }
+        public double maxVolume
+        {
+            get
+            {
+                return maxVolume;
+            }
+            set
+            {
+                maxVolume = value;
+            }
+        }
+        public double maxWeight
+        {
+            get
+            {
+                return maxWeight;
+            }
+            set
+            {
+                maxWeight = value;
+            }
+        }
+        List<Gift> gifts = new List<Gift>();
+        public Sleigh() {}
+
+        public Sleigh(string sleighName, double maxVolume, double maxWeight)
+        {
+            this.sleighName = sleighName;
+            this.maxVolume = maxVolume;
+            this.maxWeight = maxWeight;
+        }
     }
 }
